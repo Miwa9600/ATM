9e5d0d95f04c48c18140d0aee4b6e2e0ad50630b
misha <nazaryan741@gmail.com>
2023-04-05 00:20:52 +0500

    User class imp

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class User {
+    private String firstName;
+
+    private String lastName;
+
+    private String uuId; // unique universal id
+
+    private byte pinHash[];
+
+    private ArrayList<Account> accounts;
+}
