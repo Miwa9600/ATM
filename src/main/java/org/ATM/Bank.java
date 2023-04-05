737771bf10224246a608c221cb85b01fceb8dbd1
misha <nazaryan741@gmail.com>
2023-04-05 17:41:20 +0500

    Bank imp

--- src/main/java/org/ATM/Bank.java
+++ src/main/java/org/ATM/Bank.java
@@ -1,4 +1,13 @@
 package org.ATM;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+
+    private ArrayList<User> user;
+
+    private ArrayList<Account> accounts;
+
+
 }
