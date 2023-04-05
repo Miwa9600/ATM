6edf95b6f1c08afcad1eb50662151560d7aa5089
misha <nazaryan741@gmail.com>
2023-04-05 17:38:20 +0500

    Transaction imp

--- src/main/java/org/ATM/Account.java
+++ src/main/java/org/ATM/Account.java
@@ -3,15 +3,10 @@ package org.ATM;
 import java.util.ArrayList;
 
 public class Account {
-
     private String name;
-
     private double balance;
-
     private String uuid;
-
     private User holder;
-
     private ArrayList<Transaction> transactions;
 
 }
--- src/main/java/org/ATM/Transaction.java
+++ src/main/java/org/ATM/Transaction.java
@@ -1,4 +1,15 @@
 package org.ATM;
 
+import java.util.Date;
+
 public class Transaction {
+    private double amount;
+
+    private Date timestamp;
+
+    private String memo;
+
+    private Account inAccount;
+
+
 }
